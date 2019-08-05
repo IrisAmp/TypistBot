24466b136939cf15a0bd0750ed6f9c92137b4ab1
Brandon Yue <bryue@microsoft.com>
2019-08-05 13:23:30 -0500

    Updated everything to use async functions

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 TypistBot
 =========
 
-A bot that gets (very nearly) 200WPM on [keyhero.com](https://www.keyhero.com).
+A bot that types very fast on typist sites.
+- [keyhero](https://www.keyhero.com)
+- [10fastfingers](https://10fastfingers.com/typing-test/english)
 
 ## Running the bot
 Requires Chrome to be installed on your computer. Run:
@@ -15,3 +17,6 @@ Configure the bot in `bot.config.json`.
 - `username` - The username to login with
 - `password` - The password to login with
 - `numberOfRuns` - How many tests to do before closing
+- `targetWpm` - If the test website has a live WPM counter, the bot will try to stay below this WPM.
+- `slowdownChance` - The probability (between 0 and 1) that the bot will add a random slowdown to any keystroke.
+- `errorChance` - The probability (between 0 and 1) that the bot will deliberately make an error and correct it.
--- src/bot.ts
+++ src/bot.ts
@@ -1,101 +1,128 @@
 import * as Webdriver from 'selenium-webdriver';
-import { Options as ChromeOptions } from 'selenium-webdriver/chrome';
-const until = Webdriver.until;
-const By = Webdriver.By;
-const Promise = Webdriver.promise.Promise;
+const {
+  Key,
+  until,
+  By,
+} = Webdriver;
 
 const config = require('../bot.config.json');
+const {
+  username,
+  password,
+  numberOfRuns,
+  targetWpm,
+  errorChance,
+  slowdownChance,
+} = config;
 
-const botUsername = config.username;
-const botPassword = config.password;
-const numberOfRuns = config.numberOfRuns;
-
-function sendQuote(input: Webdriver.WebElement, wpmElement: Webdriver.WebElement, quote: string, doneCb: Function) {
-    if (!quote.length) {
-        doneCb();
-    } else {
-        let t0 = (new Date()).getTime();
-        input.sendKeys(quote.charAt(0)).then(() => {
-            return wpmElement.getText();
-        }).then((text => {
-            let wpm = parseFloat(text);
-            let wait = 60;
-            if (wpm < 200) {
-                wait -= (200 - wpm) + ((new Date()).getTime() - t0);
-            }
-            setTimeout(() => { sendQuote(input, wpmElement, quote.slice(1), doneCb); }, wait);
-        }));;
-    }
+async function delay(t: number) {
+  return await new Promise((r) => setTimeout(r, t));
 }
 
-function doRuns(n: number, doneCb: Function) {
-    if (n > 0) {
-        driver.findElement(By.css('.start-button')).click()
-            .then(() => {
-                return driver.findElement(By.css('.quote-current'));
-            }).then((element) => {
-                return driver.wait(until.elementTextMatches(element, /./));
-            }).then(() => {
-                return Webdriver.promise.all([
-                    driver.findElement(By.css('.quote-current')).getText(),
-                    driver.findElement(By.css('.quote-right')).getText()
-                ]);
-            }).then(texts => {
-                let quote = texts.join(' ');
-                return new Promise((resolve) => {
-                    Webdriver.promise.all([
-                        driver.findElement(By.css('input.user-input-text')),
-                        driver.findElement(By.css('[data-reactid=".0.1.1.1.0"]'))
-                    ]).then((elements) => {
-                        sendQuote(elements[0], elements[1], quote, resolve);
-                    });
-                });
-            }).then(() => {
-                return new Promise((r) => { setTimeout(r, 1000) });
-            }).then(() => {
-                driver.findElement(By.css('[data-reactid=".0.1.1.1.0"]')).getText().then(text => {
-                    console.log(`Run ${numberOfRuns - n + 1} completed (${text}WPM)`);
-                });
-                doRuns(n - 1, doneCb);
-            });
-    } else {
-        doneCb();
+async function sendSingleKeyAndDelay(input: Webdriver.WebElement, wpmElement: null | Webdriver.WebElement, key: string | number, wait?: number) {
+  await input.sendKeys(key);
+  if (wpmElement !== null && wait === undefined) {
+    wait = 100;
+    const t0 = (new Date()).getTime();
+    const wpm = parseFloat(await wpmElement.getText());
+    if (wpm < targetWpm) {
+      wait -= (targetWpm - wpm) + ((new Date()).getTime() - t0);
     }
-
+  }
+  if (Math.random() < slowdownChance) {
+    wait += (Math.random() * 150);
+  }
+  await delay(wait);
 }
 
-function doAllRuns() {
-    return new Promise((resolve) => {
-        doRuns(numberOfRuns, resolve);
-    });
+async function sendEntireQuote(input: Webdriver.WebElement, wpmElement: Webdriver.WebElement, quote: string, allowErrors = true) {
+  const originalQuote = quote;
+  while (quote.length) {
+    let nextKey = quote.charAt(0);
+    if (allowErrors && nextKey !== " " && quote.length > 5 && Math.random() < errorChance) {
+      const errorPart = " " + quote.slice(0, Math.floor(1 + (Math.random() * 3)));
+      await sendEntireQuote(input, wpmElement, errorPart, false);
+      await delay(150);
+      for (let i = 0; i < errorPart.length + 1; i++) {
+        await sendSingleKeyAndDelay(input, wpmElement, Key.BACK_SPACE);
+      }
+      const inputValue = await input.getAttribute("value");
+      quote = originalQuote.slice(inputValue.length);
+      nextKey = quote.charAt(0);
+    }
+    await sendSingleKeyAndDelay(input, wpmElement, nextKey);
+    quote = quote.slice(1);
+  }
+}
+
+async function doRuns(n: number) {
+  while (n-- > 0) {
+    await driver.findElement(By.css('.start-button')).click();
+    const element = await driver.findElement(By.css('.quote-current'));
+    await driver.wait(until.elementTextMatches(element, /./));
+    const texts = await Promise.all([
+      driver.findElement(By.css('.quote-current')).getText(),
+      driver.findElement(By.css('.quote-right')).getText()
+    ]);
+    let quote = texts.join(' ');
+    const elements = await Promise.all([
+      driver.findElement(By.css('input.user-input-text')),
+      driver.findElement(By.css('[data-reactid=".0.1.1.1.0"]'))
+    ]);
+    await delay(1000 + (Math.random() * 2000));
+    await sendEntireQuote(elements[0], elements[1], quote);
+    const resultText = await driver.findElement(By.css('[data-reactid=".0.1.1.1.0"]')).getText();
+    console.log(`Run completed (${resultText}WPM)`);
+    await delay(1000 + (Math.random() * 2000));
+  }
+}
+
+async function doAllRuns() {
+  await doRuns(numberOfRuns);
 }
 
 require('chromedriver');
 let driver = (new Webdriver.Builder()).forBrowser('chrome').build();
 
-// driver.get('https://www.keyhero.com/free-typing-test/').then(() => {
-driver.get('https://www.keyhero.com/logincreate/').then(() => {
-    return Webdriver.promise.all([
-        driver.findElement(By.css('input#id_usernamelogin')).sendKeys(botUsername),
-        driver.findElement(By.css('input#id_password')).sendKeys(botPassword)
+async function keyHero() {
+  if (username && password) {
+    await driver.get('https://www.keyhero.com/logincreate/');
+    await Promise.all([
+      driver.findElement(By.css('input#id_usernamelogin')).sendKeys(username),
+      driver.findElement(By.css('input#id_password')).sendKeys(password),
     ]);
-}).then(() => {
-    return driver.findElement(By.css('[value="Login"]')).click();
-}).then(() => {
-    return driver.wait(until.titleContains(botUsername));
-}).then(() => {
-    return driver.get('https://www.keyhero.com/free-typing-test/');
-}).then(() => {
-    // ^^^^^
-    return driver.wait(until.titleContains('Check'));
-}).then(() => {
-    return doAllRuns();
-}).then(() => {
-    return new Promise((resolve) => {
-        setTimeout(resolve, 5000);
-    });
-}).then(() => {
-    driver.close();
-}).catch((reason) => {
-    console.error(`Couldn't do the test!`, reason);
-});
+    await driver.findElement(By.css('[value="Login"]')).click();
+    await driver.wait(until.titleContains(username));
+  }
+  await driver.get('https://www.keyhero.com/free-typing-test/');
+  await driver.wait(until.titleContains('Check'));
+  await delay(15000);
+  await doAllRuns();
+  await delay(15000);
+  driver.close();
+}
+
+async function tenFastFingers() {
+  await driver.get('https://10fastfingers.com/typing-test/english');
+  await delay(5000);
+  const words = await driver.findElement(By.css('div#words'));
+  const input = await driver.findElement(By.css('input#inputfield'));
+
+  let wordnr = 0;
+  let go = true;
+  while (go) {
+    try {
+      const nextword = await words.findElement(By.css(`[wordnr="${wordnr}"]`)).getText();
+      await sendEntireQuote(input, null, nextword + " ", false);
+    } catch (ignored) {
+      go = false;
+    }
+    wordnr++;
+    go = await words.isDisplayed();
+  }
+  await delay(15000);
+  driver.close();
+}
+
+keyHero();
+tenFastFingers();
